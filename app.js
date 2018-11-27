d035ee3181c045c6892f24af6a8753e3dac49fc8
Marouane <nmraeuo@gmail.com>
2018-11-27 20:28:37 +0100

    ...

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app     = express()
-const hbs     = requiere('hbs') 
+const hbs     = require('hbs') 
+const path     = require("path")
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
   console.log(req)
+  res.render("home")
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
